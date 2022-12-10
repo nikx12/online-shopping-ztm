d12fcb56ae7387b63fb3971db396686d1096f296
nikx12 <nikhitazulka@gmail.com>
2022-12-10 14:34:21 -0500

    react router outlet learning

--- src/App.js
+++ src/App.js
@@ -1,37 +1,32 @@
+import { Route, Routes, Outlet } from 'react-router-dom';
+import Home from './routes/home';
 
-import Directory from './components/directory';
+const Shop = () => {
+  return <h1>Hello from Shop</h1>
+};
+
+const Navigation = () => {
+  return (
+  <div>
+    <h1> Navigation bar here</h1>
+    <Outlet />
+  </div>
+  )
+
+};
 
 const App = () => {
-  const categories = [
-    {
-      "id": 1,
-      "title": "hats",
-      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
-    },
-    {
-      "id": 2,
-      "title": "jackets",
-      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
-    },
-    {
-      "id": 3,
-      "title": "sneakers",
-      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
-    },
-    {
-      "id": 4,
-      "title": "womens",
-      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
-    },
-    {
-      "id": 5,
-      "title": "mens",
-      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-    }
-  ];
-
   return (
-    <Directory categories={categories} />
+    <Routes>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+      
+      </Route>
+      {/* <Outlet>
+
+      </Outlet> */}
+    </Routes>
   );
 }
 
