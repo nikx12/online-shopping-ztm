afadc08aaf805f7eff848f33a490b07b222251ed
nikx12 <nikhitazulka@gmail.com>
2023-10-24 23:37:29 -0400

    updated categories to use redux

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 import { Provider } from 'react-redux';
 import reportWebVitals from './reportWebVitals';
 // import { UserProvider } from './contexts/user.context';
-import { CategoriesProvider } from './contexts/categories.context';
+// import { CategoriesProvider } from './contexts/categories.context';
 import { CartProvider } from './contexts/cart.context';
 import { store } from './store/store';
 
@@ -16,11 +16,11 @@ root.render(
     <Provider store={store}>
       <BrowserRouter>
         {/* <UserProvider> */}
-        <CategoriesProvider>
+        {/* <CategoriesProvider> */}
           <CartProvider>
             <App />
           </CartProvider>
-        </CategoriesProvider>
+        {/* </CategoriesProvider> */}
         {/* </UserProvider> */}
       </BrowserRouter>
     </Provider>
--- src/routes/shop/index.jsx
+++ src/routes/shop/index.jsx
@@ -1,9 +1,22 @@
-import { Routes, Route } from 'react-router-dom';
 
+import { useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import CategoriesPreview from '../categories-preview';
 import Category from '../category';
+import { setCategoriesMap } from '../../store/categories/categories.action';
+import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
 
 const Shop = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoryMap = await getCategoriesAndDocuments('categories');
+      dispatch(setCategoriesMap(categoryMap));
+    }
+    getCategoriesMap();
+  }, [dispatch]);
+
   return (
     <Routes>
       <Route index element={<CategoriesPreview />} />
